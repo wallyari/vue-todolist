9b80b6733891a61fca982c5fcba4754783dba5b5
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-25 16:49:30 +0200

    Add button clearAll task

--- js/main.js
+++ js/main.js
@@ -6,24 +6,28 @@ const app = new Vue(
                 text: '',
                 done: false
             },
+            tasks: [ ],
         },
-        tasks: [ ],
-
+        
         methods: {
             addTask() {
-                // if (this.newTask.text == '') {
-                //     this.newTask.push({
-                //         text: this.newTask.text,
-                //         done: false
-                //     });
-                // } else {
-                //     this.newTask.text = '';
-                // }
+                if (this.newTask.text.trim() !== '') {
+                    this.tasks.push({
+                        text: this.newTask.text,
+                        done: false
+                    });
+                    this.newTask.text = '';
+                }
             },
-
             removeTask(index) {
-                this.tasks.splice(index, 1);
+                if (this.tasks[index] != undefined) {
+                    this.tasks.splice(index, 1);
+                }
             },
+            clearAllTasks() {
+                this.tasks = [ ];
+            },
+            
         }
     }
 );
